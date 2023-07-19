13e6f35f81e8cee0e7c85e07f6f5f153fb06f53b
Kévin B <kevinbillerach@yahoo.fr>
2023-07-19 15:03:04 +0200

    init

---
+++ Javajenkins.java
@@ -0,0 +1,9 @@
+public class Javajenkins {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, les Devops");
+        Additionner(1, 2);
+    }
+    public static int Additionner(int a, int b) {
+        return (a + b);
+    }
+}
