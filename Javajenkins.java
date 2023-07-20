cce2841d642dde4a43f7ed84f8fadb3eb3f48e07
Kévin B <kevinbillerach@yahoo.fr>
2023-07-20 14:45:04 +0200

    changes

--- Javajenkins.java
+++ Javajenkins.java
@@ -1,7 +1,7 @@
 public class Javajenkins {
     public static void main(String[] args) {
         System.out.println("Bonjour, les Devops");
-        Additionner(1, 3);
+        Additionner(1, 4);
     }
     public static int Additionner(int a, int b) {
         return (a + b);
